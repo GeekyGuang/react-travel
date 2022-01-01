a186461266bc712de9cd7f2e59d096f229bb000e
TravisWongX <travis.x.wong@gmail.com>
2022-01-01 14:29:51 +0800

    引入antd栅格系统

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,21 @@
 import styles from './App.module.css'
 import { Header, Footer } from './components'
+import { Row, Col } from 'antd'
 
 function App() {
   return (
     <div className={styles.App}>
       <Header />
+      <div className={styles['page-content']}>
+        <Row style={{ marginTop: 20 }}>
+          <Col span={6}>
+            <div style={{ background: 'red' }}>多重菜单</div>
+          </Col>
+          <Col span={18}>
+            <div style={{ background: 'blue' }}>走马灯</div>
+          </Col>
+        </Row>
+      </div>
       <Footer />
     </div>
   )
