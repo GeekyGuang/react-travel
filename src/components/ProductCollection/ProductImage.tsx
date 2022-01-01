614933e797381459f0004b9a94c35b8035a45bf5
TravisWongX <travis.x.wong@gmail.com>
2022-01-01 23:49:13 +0800

    使用Link

--- src/components/ProductCollection/ProductImage.tsx
+++ src/components/ProductCollection/ProductImage.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { Image, Typography } from 'antd'
-import { useNavigate } from 'react-router'
+import { Link } from 'react-router-dom'
 
 interface PropsType {
   id: string | number
@@ -17,9 +17,8 @@ export const ProductImage: React.FC<PropsType> = ({
   price,
   title,
 }) => {
-  const navigate = useNavigate()
   return (
-    <div onClick={() => navigate(`/detail/${id}`)}>
+    <Link to={`/detail/${id}`}>
       {size === 'large' ? (
         <Image src={imageSrc} height={285} width={490} />
       ) : (
@@ -31,6 +30,6 @@ export const ProductImage: React.FC<PropsType> = ({
           ¥ {price} 起
         </Typography.Text>
       </div>
-    </div>
+    </Link>
   )
 }
