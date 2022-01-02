f9388455f68a62604ac632292f1bc3fb0a1d5f4e
TravisWongX <travis.x.wong@gmail.com>
2022-01-02 19:53:08 +0800

    实现语言切换

--- src/redux/languageReducer.ts
+++ src/redux/languageReducer.ts
@@ -1,3 +1,5 @@
+import i18n from 'i18next'
+
 interface LanguageState {
   language: 'en' | 'zh'
   languageList: { name: string; code: string }[]
@@ -14,6 +16,7 @@ const defaultState: LanguageState = {
 const languageReducer = (state = defaultState, action) => {
   switch (action.type) {
     case 'change_language':
+      i18n.changeLanguage(action.payload)
       return { ...state, language: action.payload }
     case 'add_language':
       return {
