b1eb2ae527fcf65cfe58dfd20a8042cd5eafffaf
TravisWongX <travis.x.wong@gmail.com>
2022-01-02 17:00:16 +0800

    if 改成 switch

--- src/redux/languageReducer.ts
+++ src/redux/languageReducer.ts
@@ -11,18 +11,18 @@ const defaultState: LanguageState = {
   ],
 }
 
-export default (state = defaultState, action) => {
-  if (action.type === 'change_language') {
-    const newState = { ...state, language: action.payload }
-    return newState
+const languageReducer = (state = defaultState, action) => {
+  switch (action.type) {
+    case 'change_language':
+      return { ...state, language: action.payload }
+    case 'add_language':
+      return {
+        ...state,
+        languageList: [...state.languageList, action.payload],
+      }
+    default:
+      return state
   }
-
-  if (action.type === 'add_language') {
-    const newState = {
-      ...state,
-      languageList: [...state.languageList, action.payload],
-    }
-    return newState
-  }
-  return state
 }
+
+export default languageReducer
