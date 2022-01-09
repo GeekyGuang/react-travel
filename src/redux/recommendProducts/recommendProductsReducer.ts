da59905bd799f2cd86530bb93b70db405d953c94
travis <travis.x.wong@gmail.com>
2022-01-09 23:57:33 +0800

    修改recommendProductsReducer

--- src/redux/recommendProducts/recommendProductsReducer.ts
+++ src/redux/recommendProducts/recommendProductsReducer.ts
@@ -1,3 +1,4 @@
+import { FETCH_RECOMMEND_PRODUCTS_FAIL, RecommendProductsAction, FETCH_RECOMMEND_PRODUCTS_START, FETCH_RECOMMEND_PRODUCTS_SUCCESS } from './recommendProductsActions';
 interface RecommendProductsState {
     loading: boolean;
     error: string | null,
@@ -10,8 +11,18 @@ const defaultState: RecommendProductsState  = {
     productList: []
 }
 
-const recommendProductsReducer = (state = defaultState, action) => {
-  return state
+const recommendProductsReducer = (state = defaultState, action: RecommendProductsAction) => {
+    switch(action.type){
+        case FETCH_RECOMMEND_PRODUCTS_START:
+            return {...state, loading: true}
+        case FETCH_RECOMMEND_PRODUCTS_SUCCESS:
+            return {...state, loading: false, productList: action.payload}
+        case FETCH_RECOMMEND_PRODUCTS_FAIL:
+            return {...state, loading: false, error: action.payload}
+        default: 
+            return state
+    }
+
 }
 
 export default recommendProductsReducer
