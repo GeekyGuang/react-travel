02232de9d988664d5d39f99f1566dd8efdc1adf7
travis <travis.x.wong@gmail.com>
2022-01-19 07:28:52 +0800

    添加注册表单

--- src/pages/register/Register.tsx
+++ src/pages/register/Register.tsx
@@ -1,11 +1,12 @@
 import { UserLayout } from "../../layouts/userLayout"
+import { RegisterForm } from "./RegisterForm"
 
 
 export const RegisterPage = (props) => {
   
   return <>
     <UserLayout>
-    <h1>注册页面</h1>
+    <RegisterForm />
     </UserLayout>
   </>
 }
