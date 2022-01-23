6682b28ab7e1e424516463551ef3567b48c20432
TravisWongX <travis.x.wong@gmail.com>
2022-01-23 15:06:20 +0800

    redux-persist 登录持久化

--- src/index.tsx
+++ src/index.tsx
@@ -4,17 +4,20 @@ import './index.css'
 import App from './App'
 import 'antd/dist/antd.css'
 import './i18n/configs'
-import {Provider} from 'react-redux'
-import store from './redux/store'
+import { Provider } from 'react-redux'
 import axios from 'axios'
-axios.defaults.baseURL = 'https://www.fastmock.site/mock/c950a0853fadaff628acd99e4a8f2567/travel'
+import rootStore from './redux/store'
+import { PersistGate } from 'redux-persist/integration/react'
+axios.defaults.baseURL =
+  'https://www.fastmock.site/mock/c950a0853fadaff628acd99e4a8f2567/travel'
 
 ReactDOM.render(
   <React.StrictMode>
-    <Provider store={store}>
-       <App />
+    <Provider store={rootStore.store}>
+      <PersistGate loading={null} persistor={rootStore.persistor}>
+        <App />
+      </PersistGate>
     </Provider>
-
   </React.StrictMode>,
   document.getElementById('root')
 )
--- src/redux/store.ts
+++ src/redux/store.ts
@@ -6,6 +6,14 @@ import thunk from 'redux-thunk'
 import { actionLog } from './middlewares/actionLog'
 import { productDetailSlice } from './productDetail/slice'
 import { combineReducers, configureStore } from '@reduxjs/toolkit'
+import { persistStore, persistReducer } from 'redux-persist'
+import storage from 'redux-persist/lib/storage' // 默认是localStorage
+
+const persistConfig = {
+  key: 'root',
+  storage,
+  whitelist: ['user'], // 即rootReducer中的user, 在白名单中的才会存储到localStorage
+}
 
 const rootReducer = combineReducers({
   language: languageReducer,
@@ -14,13 +22,16 @@ const rootReducer = combineReducers({
   user: userSlice.reducer,
 })
 
-// const store = createStore(rootReducer, applyMiddleware(thunk, actionLog))
+const persistedReducer = persistReducer(persistConfig, rootReducer)
+
 const store = configureStore({
-  reducer: rootReducer,
+  reducer: persistedReducer, // 使用 persistedReducer 替代 rootReducer
   middleware: (getDefaultMiddleware) => [...getDefaultMiddleware(), actionLog],
   devTools: true,
 })
 
+const persistor = persistStore(store)
+
 export type RootState = ReturnType<typeof store.getState>
 export type AppDispatch = typeof store.dispatch
-export default store
+export default { store, persistor }
